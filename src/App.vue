d7a123d1b58bd7056a0b958defc79beceb6849c4
otake <otake>
2025-06-06 16:18:38 +0900

    カードを並べる

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import card from './components/card5.vue'
+const cards = [
+  {
+    name: 'ヒトカゲ',
+    energy: '5',
+    ability: '2回復',
+    type: '炎',
+    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
+  },
+  {
+    name: 'ゼニガメ',
+    energy: '3',
+    ability: '防御力アップ',
+    type: '水',
+    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
+  },
+  {
+    name: 'フシギダネ',
+    energy: '4',
+    ability: '回復5',
+    type: '草',
+    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
+  }
+]
+
+
+
+
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<template>
+  <card :cards="cards"></card>
+</template>
---
+++ src/components/card5.vue
@@ -0,0 +1,32 @@
+<script setup>
+import cards from "./card3.vue"
+import card from "./card.vue"
+defineProps({
+  cards: Object
+})
+</script>
+
+<template>
+    <div class="w-140 h-120 border border-gray-300 rounded-xl p-4 shadow-sm bg-white space-y-15">
+    <div class="flex items-center space-x-4">
+      <h2 class="text-lg font-semibold">1</h2>
+      <card :cards="cards"></card> 
+    </div>
+    <div class="flex items-center space-x-4">
+      <h2 class="text-lg font-semibold">2</h2>
+      <card :cards="cards"></card> 
+    </div>
+    <div class="flex items-center space-x-4">
+      <h2 class="text-lg font-semibold">3</h2>
+      <card :cards="cards"></card> 
+    </div>
+    <div class="flex items-center space-x-4">
+      <h2 class="text-lg font-semibold">4</h2>
+      <card :cards="cards"></card> 
+    </div>
+    <div class="flex items-center space-x-4">
+      <h2 class="text-lg font-semibold">6</h2>
+      <card :cards="cards"></card> 
+    </div>
+  </div>
+</template>
