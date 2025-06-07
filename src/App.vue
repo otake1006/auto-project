ee0bb82affc2053490dfc315820c51699bed7885
otake1006 <san-j23008@sangi.jp>
2025-06-07 18:56:32 +0900

    親子

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <script setup>
 import { ref } from 'vue'
 import card from './components/card5.vue'
-const cards = [
-  {
-    name: 'ヒトカゲ',
-    energy: '5',
-    ability: '2回復',
-    type: '炎',
-    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
+const parentList = ref([
+ {
+   id: 1,
+   name: '親1',
+   children: [
+   { id: 'a', name: 'スキル', energy: "1" },
+   { id: 'b', name: 'ファイヤ' }
+   ]
   },
   {
-    name: 'ゼニガメ',
-    energy: '3',
-    ability: '防御力アップ',
-    type: '水',
-    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
-  },
-  {
-    name: 'フシギダネ',
-    energy: '4',
-    ability: '回復5',
-    type: '草',
-    imgSrc: 'https://media.discordapp.net/attachments/1331433261716475925/1380186664227569837/8c45cf85becf84af9de3df62bb84a767.png?ex=68439f3f&is=68424dbf&hm=4e28312f6ab8c2f0f00bcb56964978aadd2fb3b312564601db3f117c9fb365eb&=&format=webp&quality=lossless&width=450&height=450'
+    id: 2,
+    name: '親2',
+    children: [
+      { id: 'c', name: '子2a' },
+      { id: 'd', name: '子2b' }
+    ]
   }
-]
+])
 
 
 
