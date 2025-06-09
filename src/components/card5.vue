de0695f798ca2ebe9439911d0419d2c74a358f71
ei70 <san-j23005@sangi.jp>
2025-06-10 08:45:00 +0900

    wip

--- src/components/card5.vue
+++ src/components/card5.vue
@@ -2,33 +2,30 @@
 import { ref } from 'vue'
 import card3 from "./card.vue"
 import draggable from "vuedraggable"
+import { useSkillStore } from '@/stores/skillStore'
+
+const skillStore = useSkillStore();
 const props = defineProps({
-  parentList: Object
+    parentList: Object
 })
 const parent = ref(props.parentList)
 </script>
 
 <template>
-<div class="border border-gray-300 rounded-xl p-4 shadow-sm bg-white space-y-15">
- <draggable v-model="parent" group="parent" item-key="id">
-   <template #item="{ element }">
-     <div class="parent-box flex">
-        <p>親:{{ element.name }}</p>
-       <draggable
-         v-model="element.children"
-         group="child"
-         item-key="id"
-         class="flex"
-        >
-         <template #item="{ element: child}">
-            <div class="child-box">
-              <card3 :cards="child"></card3>
+    <div class="border border-gray-300 rounded-xl p-4 shadow-sm bg-white space-y-15">
+        <draggable v-model="skillStore.itemList" group="parent" item-key="index">
+            <template #item="{ element: slot, index }">
+                <div class="parent-box flex">
+                    <h3 class="font-semibold">{{ index + 1 }}</h3>
+                    <draggable v-model="skillStore.itemList[index]" group="skills" item-key="id" class="flex">
+                        <template #item="{ element: child }">
+                            <div class="child-box">
+                                <card3 :cards="child"></card3>
+                            </div>
+                        </template>
+                    </draggable>
                 </div>
-         </template>
+            </template>
         </draggable>
-        </div>
-     </template>
-    </draggable>
     </div>
-
-  </template>
+</template>
