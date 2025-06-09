90dfcb1721831c97ab2ac844a0f34a922bb84538
ei70 <san-j23005@sangi.jp>
2025-06-10 08:45:00 +0900

    wip

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="flex-1 overflow-y-auto border-2 border-white py-2 rounded-lg min-h-[200px]">
-    <Card v-for="card in cards" :key="card.id" :card="card" />
-  </div>
+    <div class="flex-1 overflow-y-auto border-2 border-white py-2 rounded-lg min-h-[200px]">
+        <draggable :list="cards" :group="{ name: 'skills' }" item-key="id" :sort="false" :animation="200">
+            <template #item="{ element }">
+                <Card :card="element" />
+            </template>
+        </draggable>
+    </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
 import Card from './CardItem.vue'
+import draggable from 'vuedraggable'
+
 
 const props = defineProps({
-  cards: Array
+    cards: Array
 })
 </script>
