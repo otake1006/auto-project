7b9df6530859eba4f0d023dc6c4cd8d38ce6c058
otake <otake>
2025-06-10 17:23:57 +0900

    battleStore表示

--- src/components/BattleStatus.vue
+++ src/components/BattleStatus.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="flex flex-1 flex-row items-center justify-around p-4">
-        <CharacterStatus icon="/assets/player.png" :hp="20" :maxHp="20" :energy="6" :maxEnergy="6" />
-        <CharacterStatus icon="/assets/monster.png" :hp="15" :maxHp="20" :energy="3" :maxEnergy="6" />
+        <CharacterStatus icon="/assets/player.png" :hp="battle.player.hp" :maxHp="battle.player.maxhp" :energy="battle.player.mp" :maxEnergy="battle.player.maxmp" />
+        <CharacterStatus icon="/assets/monster.png" :hp="battle.monster.hp" :maxHp="battle.monster.maxhp" :energy="battle.monster.mp" :maxEnergy="battle.monster.maxmp" />
     </div>
 </template>
 
 <script setup>
     import CharacterStatus from './CharacterStatus.vue'
+    import {useBattleStore} from '@/stores/battleStore'
+    const battle = useBattleStore();
 </script>
