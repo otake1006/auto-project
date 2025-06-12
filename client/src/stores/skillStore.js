e7b228c905b695d43268b2f8b073bfe2785e97d6
ei70 <san-j23005@sangi.jp>
2025-06-12 21:45:40 +0900

    ref wip

--- client/src/stores/skillStore.js
+++ client/src/stores/skillStore.js
@@ -22,11 +22,53 @@ export const useSkillStore = defineStore('skill', () => {
 
     const itemList = ref([[], [], [], [], []]);
 
+    const skillSets = ref([
+        {
+            id: 'set1',
+            skill: null,
+            conditions: [],
+        },
+        {
+            id: 'set2',
+            skill: null,
+            conditions: [],
+        },
+        {
+            id: 'set3',
+            skill: null,
+            conditions: [],
+        },
+        {
+            id: 'set4',
+            skill: null,
+            conditions: [],
+        },
+        {
+            id: 'set5',
+            skill: null,
+            conditions: [],
+        },
+    ]);
+
+    function handleSkillAdd(event, index) {
+        // 追加されたスキルを取得
+        const addedSkill =
+            skillSets.value[index].skill ||
+            (event.item &&
+                event.item.__draggable_context &&
+                event.item.__draggable_context.element);
+        if (addedSkill && !skillSets.value[index].skill) {
+            skillSets.value[index].skill = addedSkill;
+        }
+    }
+
     // 実際には type でフィルタする（例: 'skill', 'condition', 'relic' など）
     const filteredCards = computed(() => {
         const type = tabTypeMap[currentTab.value];
         return cards.filter((card) => card.type === type);
     });
+
+    const currentType = computed(() => tabTypeMap[currentTab.value]);
 
     function setTab(tab) {
         currentTab.value = tab;
@@ -35,10 +77,13 @@ export const useSkillStore = defineStore('skill', () => {
     return {
         player,
         itemList,
+        skillSets,
         cards,
         tabs,
+        currentType,
         currentTab,
         filteredCards,
+        handleSkillAdd,
         setTab,
     };
 });
