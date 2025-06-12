a8197490179459fef25b3da0695861388e9e5a7d
ei70 <san-j23005@sangi.jp>
2025-06-12 21:45:40 +0900

    ref wip

--- client/src/components/CardList.vue
+++ client/src/components/CardList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex-1 overflow-y-auto border-2 border-white py-2 rounded-lg min-h-[200px]">
-        <draggable :list="cards" :group="{ name: 'skills' }" item-key="id" :sort="false" :animation="200">
+        <draggable :list="cards" :group="{ name: skill.currentType }" item-key="id" :sort="false" :animation="200">
             <template #item="{ element }">
                 <Card :card="element" />
             </template>
@@ -12,7 +12,9 @@
 import { defineProps } from 'vue'
 import Card from './CardItem.vue'
 import draggable from 'vuedraggable'
+import { useSkillStore } from '@/stores/skillStore'
 
+const skill = useSkillStore();
 
 const props = defineProps({
     cards: Array
