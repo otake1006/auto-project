abb756c4039976220759d60fd8ffefb33bdbce94
Elic0de <san-j23005@sangi.jp>
2025-06-18 21:36:25 +0900

    feat: adjust card5 style and add draggable delete logic in skillStore

--- client/src/components/card5.vue
+++ client/src/components/card5.vue
@@ -9,19 +9,48 @@ const props = defineProps({
     parentList: Object,
 });
 const parent = ref(props.parentList);
+const dropArea = ref(null)
+
+function onDragEnd(evt) {
+    const mouseX = evt.originalEvent.clientX
+    const mouseY = evt.originalEvent.clientY
+    const dropRect = dropArea.value.getBoundingClientRect()
+    const isOutside =
+        mouseX < dropRect.left ||
+        mouseX > dropRect.right ||
+        mouseY < dropRect.top ||
+        mouseY > dropRect.bottom
+
+    if (isOutside) {
+        const index = evt.oldIndex
+        if (index !== undefined) {
+            skillStore.handleSkillRemove(index);
+        }
+    }
+
+}
+
+
 </script>
 
 <template>
-    <div class="space-y-15 rounded-xl border border-gray-300 bg-white p-4 shadow-sm">
-        <draggable v-model="skillStore.skillSets" item-key="id" :group="{ name: 'skillSets', put: false }"
-            :ghost-class="'no-opacity'">
+    <div class="space-y-15 rounded-xl border border-gray-300 bg-white p-4 shadow-sm" ref="dropArea">
+        <draggable v-model="skillStore.skillSets" item-key="id" :group="{ name: 'skillSets' }"
+            :ghost-class="'no-opacity'" :animation="200" handle=".drag-handle">
             <template #item="{ element, index }">
-                <div class="flex w-full p-4 py-2 flex-row gap-6">
-                    <h3 class="font-semibold">{{ index + 1 }}</h3>
-                    <draggable :list="element.skill ? [element.skill] : []" :group="{ name: 'skill' }" item-key="id"
-                        :ghost-class="'no-opacity'" @add="(e) => skillStore.handleSkillAdd(e, index)">
+                <div class="flex w-full p-4 py-2 flex-row gap-6 h-25">
+                    <h3 class="font-semibold drag-handle">{{ index + 1 }}</h3>
+                    <draggable :list="element.skill ? [element.skill] : []" :group="{ name: 'skill', pull: false }"
+                        item-key="id" :ghost-class="'no-opacity'" @add="(e) => skillStore.handleSkillAdd(e, index)"
+                        @end="onDragEnd" @remove="(e) => skillStore.handleSkillRemove(index)">
                         <template #item="{ element: skill }">
                             <card3 :cards="skill"></card3>
+                        </template>
+                        <template #footer>
+                            <div v-if="!element.skill"
+                                class="flex items-center justify-center  w-48 h-20 border-2 border-dashed border-gray-400 rounded bg-gray-50 text-gray-500 italic">
+                                + ここにカードを追加
+                            </div>
                         </template>
                     </draggable>
 
@@ -30,6 +59,7 @@ const parent = ref(props.parentList);
                             <card3 :cards="condition"></card3>
                         </template>
                     </draggable>
+
                 </div>
             </template>
         </draggable>
