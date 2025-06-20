bc4de98fe1ca45419081cb701f61616653ef3db2
Elic0de <lpondboy@gmail.com>
2025-06-20 09:57:38 +0900

    refactor(ui): remove BattleStatus component from main layout
    - Removed BattleStatus import and usage from the main template
    - Simplified layout by focusing on tactics and skill selection areas

--- client/src/components/BoardSection.vue
+++ client/src/components/BoardSection.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="flex h-screen w-screen flex-col overflow-hidden bg-green-200">
         <!-- 上部：バトル -->
-        <BattleStatus />
 
         <!-- 下部：作戦＆カード選択 -->
         <div class="flex flex-[4] overflow-hidden border-t-2 border-gray-300 bg-neutral-700 p-4">
@@ -12,7 +11,6 @@
 </template>
 
 <script setup>
-import BattleStatus from './BattleStatus.vue'
 import TacticsBoard from './TacticsBoard.vue'
 import SkillSelection from './SkillSelection.vue'
 
