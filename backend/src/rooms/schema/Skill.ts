991a0228160d3b56ea183bf27de7b1c338abfa91
otake <otake>
2025-06-23 22:25:48 +0900

    conditionの型変更

--- backend/src/rooms/schema/Skill.ts
+++ backend/src/rooms/schema/Skill.ts
@@ -1,6 +1,6 @@
 import { Schema, MapSchema, type, ArraySchema } from '@colyseus/schema';
 export class Condition extends Schema {
-    @type('string') type: string;
+    @type('number') id: number;
     @type('number') value: number;
 }
 export class Skill extends Schema {
