8ce9a126168f54873fc8d06f854ce465a4078875
Elic0de <lpondboy@gmail.com>
2025-06-25 20:59:19 +0900

    feat(drag-drop): enable access to skillSet index via data attribute in move hook

--- client/src/components/card5.vue
+++ client/src/components/card5.vue
@@ -32,31 +32,11 @@ function onDragEnd(evt) {
 }
 
 function canMove(evt) {
+    console.log('canMove called', evt);
     const draggedElement = evt.relatedContext.element;
     const contextElement = evt.draggedContext.element;
 
     return draggedElement.skill !== null && contextElement.skill !== null;
-}
-
-
-
-function handleChange(evt) {
-    if (evt.added && evt.added.element) {
-        const newItem = evt.added.element;
-        console.log('New item added:', evt);
-
-        // 追加対象リストを取得（componentごとにドラッグ先が異なる場合は要工夫）
-        const toList = evt.to.__draggable_component?.modelValue;
-        const index = evt.added.newIndex;
-
-        // Vue 3 では .value や $set 不要、splice で OK
-        if (toList) {
-            toList.splice(index, 1, {
-                ...newItem,
-                value: 0  // ← 任意の値に書き換え
-            });
-        }
-    }
 }
 
 
@@ -67,7 +47,7 @@ async function onDropped(data, index) {
         const newIndex = data.newIndex;
         skillStore.skillSets[index].conditions[newIndex] = {
             ...data.item.__draggable_context.element,
-            value: inputValue  // ← ここで書き換え！
+            value: inputValue
         };
     }
 
@@ -96,9 +76,9 @@ async function onDropped(data, index) {
                         </template>
                     </draggable>
 
-
                     <draggable v-model="element.conditions" :ghost-class="'ghost'" :group="{ name: 'condition' }"
-                        @add="(e) => onDropped(e, index)" item-key="id" class="flex gap-6">
+                        data-group="condition" :data-index="index" @add="(e) => onDropped(e, index)" item-key="id"
+                        class="flex gap-6" :move="canMove">
                         <template #item="{ element: condition }">
                             <card3 :cards="condition"></card3>
                         </template>
