9e063d892c64ac6aa165ea47acd5eac4064576b1
otake1006 <san-j23008@sangi.jp>
2025-07-02 10:01:17 +0900

    リザルト画面の背景追加

--- client/src/scenes/ResultScene.js
+++ client/src/scenes/ResultScene.js
@@ -3,15 +3,32 @@ export class ResultScene extends Phaser.Scene {
         super('ResultScene');
     }
 
+    preload() {
+        this.load.spritesheet('bgAnim', 'UntitledArtwork3.png', {
+            frameWidth: 1440,
+            frameHeight: 810,
+        });
+    }
+
     init(data) {
         this.winner = data?.winner || '不明';
     }
 
     create() {
-        const displayText = this.winner === 'draw' ? '相打ち！' : `勝者: ${this.winner}`;
+        const displayText = this.winner === 'draw' ? '相打ち！' : `${this.winner}`;
         // 背景色などが必要であれば追加してください
+
+        this.anims.create({
+            key: 'bg-loop',
+            frames: this.anims.generateFrameNumbers('bgAnim'),
+            frameRate: 8, // フレーム速度
+            repeat: -1, // ループ
+        });
+
+        this.add.sprite(0, 0, 'bgAnim').setOrigin(0).setDisplaySize(1440, 810).play('bg-loop');
+
         this.add
-            .text(720, 300, displayText, {
+            .text(900, 300, displayText, {
                 fontSize: '48px',
                 color: '#ffffff',
             })
