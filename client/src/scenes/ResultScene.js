ae01f88b66a28f9c246c67981c50355a1f8fb0e1
otake1006 <san-j23008@sangi.jp>
2025-07-02 17:35:11 +0900

    照明追加

--- client/src/scenes/ResultScene.js
+++ client/src/scenes/ResultScene.js
@@ -4,7 +4,7 @@ export class ResultScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('bgAnim', 'UntitledArtwork3.png', {
+        this.load.spritesheet('result', 'result.png', {
             frameWidth: 1440,
             frameHeight: 810,
         });
@@ -21,23 +21,23 @@ export class ResultScene extends Phaser.Scene {
         // 背景色などが必要であれば追加してください
 
         this.anims.create({
-            key: 'bg-loop',
-            frames: this.anims.generateFrameNumbers('bgAnim'),
+            key: 'loop',
+            frames: this.anims.generateFrameNumbers('result'),
             frameRate: 8, // フレーム速度
             repeat: -1, // ループ
         });
 
-        this.add.sprite(0, 0, 'bgAnim').setOrigin(0).setDisplaySize(1440, 810).play('bg-loop');
+        this.add.sprite(0, 0, 'result').setOrigin(0).setDisplaySize(1440, 810).play('loop');
 
         this.add
-            .text(900, 300, displayText, {
+            .text(1200, 400, displayText, {
                 fontSize: '48px',
                 color: '#ffffff',
             })
             .setOrigin(0.5);
 
         this.add
-            .text(720, 500, 'タイトルに戻る', {
+            .text(1100, 600, 'タイトルに戻る', {
                 fontSize: '32px',
                 backgroundColor: '#0000aa',
                 color: '#ffffff',
