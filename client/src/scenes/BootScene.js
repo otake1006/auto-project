b369f94b75a7cb54376f4f422f425bf0680e68a3
otake1006 <san-j23008@sangi.jp>
2025-07-04 18:13:03 +0900

    ロード画面読み込み

--- client/src/scenes/BootScene.js
+++ client/src/scenes/BootScene.js
@@ -4,9 +4,20 @@ export class BootScene extends Phaser.Scene {
         super('BootScene');
     }
 
-    preload() {}
+    preload() {
+        this.load.spritesheet('load', 'Loading.png', {
+            frameWidth: 1440,
+            frameHeight: 810,
+        });
+    }
 
     create() {
+        this.anims.create({
+            key: 'loading',
+            frames: this.anims.generateFrameNumbers('load'),
+            frameRate: 8, // フレーム速度
+            repeat: -1, // ループ
+        });
         this.scene.start('PreloadScene');
     }
 }
