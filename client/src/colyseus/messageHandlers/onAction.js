7c67c9062896d8d4e4e1cf2c1f6956d87aa85a63
Elic0de <lpondboy@gmail.com>
2025-07-09 21:40:56 +0900

    chore: Remove deprecated onAction message handler

--- client/src/colyseus/messageHandlers/onAction.js
+++
@@ -1,7 +0,0 @@
-// src/colyseus/messageHandlers/onAction.js
-import { useSkillStore } from '@/stores/skillStore';
-
-export function onAction(_room, skills) {
-    const skillStore = useSkillStore();
-    skillStore.setSkills(skills);
-}
