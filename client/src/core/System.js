d5153e476efe815031ed948780f512ea290d3ca3
Elic0de <lpondboy@gmail.com>
2025-07-10 06:16:55 +0900

    feat: Introduce base System class for ECS

---
+++ client/src/core/System.js
@@ -0,0 +1,8 @@
+export class System {
+    /** @returns true を返すエンティティだけ update に渡される */
+    filter(entity) {
+        return false;
+    }
+    /** @param {number} dt deltaTime(ms) */
+    update(dt, entities) {}
+}
