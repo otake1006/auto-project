a219aebd70630f5e1bb5b9820a4c9e06e5dc3ea5
Elic0de <lpondboy@gmail.com>
2025-07-09 21:43:22 +0900

    chore: Remove deprecated onAction message handler file

--- client/src/colyseus/messageHandlers/onCondition.js
+++
@@ -1,6 +0,0 @@
-import { useSkillStore } from '@/stores/skillStore';
-
-export function onCondition(conditions) {
-    const skillStore = useSkillStore();
-    skillStore.loadConditionFromColyseus(conditions);
-}
