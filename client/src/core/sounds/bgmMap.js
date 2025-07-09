b22a6d6fc496c0087821586ad4c1943b84487127
Elic0de <lpondboy@gmail.com>
2025-07-10 06:25:58 +0900

    refactor: Update BGM scene mapping

--- client/src/core/sounds/bgmMap.js
+++ client/src/core/sounds/bgmMap.js
@@ -1,5 +1,5 @@
 // core/sounds/bgmMap.js
 export const bgmMap = {
     StartScene: { key: 'start_bgm', file: 'the-return-of-the-8-bit-era-301292.mp3', loop: true },
-    BattleScene: { key: 'battle_bgm', file: 'pixel-245147.mp3', loop: true },
+    GameScene: { key: 'battle_bgm', file: 'pixel-245147.mp3', loop: true },
 };
