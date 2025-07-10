ce4235d269978a3bb0158279a5ae7fa934bea620
Elic0de <lpondboy@gmail.com>
2025-07-10 18:46:26 +0900

    feat: Add ScenePhaseSystem for managing scene transitions and background music

---
+++ client/src/systems/ScenePhaseSystem.js
@@ -0,0 +1,31 @@
+import { System } from '@/core/System.js';
+import { phaserEvents, Event } from '@/events/EventCenter';
+
+export class ScenePhaseSystem extends System {
+    constructor(scene, bgm) {
+        super();
+        this.scene = scene;
+        this.bgm = bgm;
+        phaserEvents.on('scene-change', (d) => this._toResult(d));
+        phaserEvents.on('leave-room', () => this._toStart());
+    }
+
+    _toResult(data) {
+        this.bgm.fadeOut(500, () => this.scene.start('ResultScene', data));
+    }
+
+    _toStart() {
+        this.bgm.fadeOut(500, () => this.scene.start('StartScene'));
+    }
+
+    filter() {
+        return false;
+    }
+
+    update() {}
+
+    destroy() {
+        phaserEvents.off('scene-change');
+        phaserEvents.off('leave-room');
+    }
+}
