2c9200aa2ca096ca5f05ea6ddaaa45b8baa4b014
otake1006 <san-j23008@sangi.jp>
2025-07-11 15:52:19 +0900

    脆弱と弱体のデバフをbooleanからnumberに

--- backend/src/rooms/schema/buff.ts
+++ backend/src/rooms/schema/buff.ts
@@ -2,7 +2,7 @@ import { Schema, type } from '@colyseus/schema';
 export class buff extends Schema {
     @type('number') muscular: number = 0; //筋肉
     @type('number') guard: number = 0; //ガード値
-    @type('boolean') brittle: boolean = false; //脆い
-    @type('boolean') Weak: boolean = false; //弱体
+    @type('number') brittle: number = 0; //脆い
+    @type('number') Weak: number = 0; //弱体
     @type('number') poison: number = 0; //毒
 }
