6492771e93941729f7643273f190b9d6c62860db
otake1006 <san-j23008@sangi.jp>
2025-07-11 10:59:43 +0900

    ファイル名変更

--- backend/src/rooms/schema/MyRoomState.ts
+++ backend/src/rooms/schema/MyRoomState.ts
@@ -1,20 +1,21 @@
 import { Schema, MapSchema, type, ArraySchema } from '@colyseus/schema';
-import { Condition, Skill } from './Skill';
+import { Skill } from './Skill';
 import { buff } from './buff';
-import { getSkillCard, getCondition, SkillCard, skillCards } from '../../data/card';
+import { GameConfig } from '../../config/game';
+import { SkillCard } from '../../data/skill';
 
 export class Player extends Schema {
     @type('string') name: string = 'player';
     @type('number') hp: number = 0;
     @type('number') mp: number = 0;
     @type('number') shield: number = 0;
-    @type('number') maxhp: number = 100;
-    @type('number') maxmp: number = 50;
-    @type('number') maxshield: number = 100;
-    @type('number') ratiohp: number = 100;
-    @type('number') ratiomp: number = 100;
+    @type('number') maxhp: number = GameConfig.PLAYER_MAX_HP;
+    @type('number') maxmp: number = GameConfig.PLAYER_MAX_MP;
+    @type('number') maxshield: number = GameConfig.PLAYER_MAX_SHIELD;
+    @type('number') ratiohp: number = GameConfig.PLAYER_RATIO_HP;
+    @type('number') ratiomp: number = GameConfig.PLAYER_RATIO_MP;
     @type('boolean') ready: boolean = false;
-    @type([Skill]) skill = new ArraySchema<Skill>();
+    @type([Skill]) skill = new ArraySchema<Skill>(); //作戦ボード内のスキル
     @type(buff) buffs = new buff();
     reset() {
         this.hp = this.maxhp;
@@ -34,62 +35,19 @@ export class Player extends Schema {
     resetShield() {
         this.shield = 0;
     }
-
-    selectSkill() {
-        const context = { hp: this.hp, mp: this.mp };
-        for (const set of this.skill) {
-            if (
-                set.skill &&
-                this.mp >= getSkillCard(set.skill).energy &&
-                this.evaluateAllConditions(set.conditions, context)
-            ) {
-                return set.skill;
-            }
-        }
-        return null;
-    }
-
-    useSkill(skillId: number, target: Player) {
-        if (!skillId) return;
-        const skill = getSkillCard(skillId);
-        const damage = skill.damage * skill.Count;
-        if (skill.battleType === 'attack') {
-            this.mp -= skill.energy;
-            const HPdamage = Math.max(0, damage - target.shield);
-            target.shield = Math.max(0, target.shield - damage);
-            target.hp = Math.max(0, target.hp - HPdamage);
-        }
-        if (skill.battleType === 'defense') {
-            this.mp -= skill.energy;
-            this.shield = Math.min(this.maxshield, this.shield + damage);
-        }
-    }
-
-    evaluateCondition(condition: Condition, context: any) {
-        if (!getCondition(condition)) return true;
-
-        const Condition = getCondition(condition);
-        Condition.value = condition.value;
-        switch (Condition.conditionType) {
-            case 'HP_ABOVE':
-                return context.hp >= Condition.value;
-            case 'HP_BELOW':
-                return context.hp <= Condition.value;
-            case 'MP_ABOVE':
-                return context.mp >= Condition.value;
-            case 'MP_BELOW':
-                return context.mp <= Condition.value;
-            // 追加条件にも対応可能
-            default:
-                return false;
-        }
-    }
-
-    evaluateAllConditions(conditions: ArraySchema, context: any) {
-        return conditions.every((cond) => this.evaluateCondition(cond, context));
-    }
 }
-
 export class MyRoomState extends Schema {
     @type({ map: Player }) players = new MapSchema<Player>();
+    @type('string') gameState: string = 'ready';
+    @type('string') winner: string = 'draw';
+    @type('number') winCount1: number = 0;
+    @type('number') winCount2: number = 0;
+    @type('number') drawCount: number = 0;
+    @type('number') round: number = 1;
+    @type('number') turn: number = 1;
+    @type([SkillCard]) initialSkill = new ArraySchema<SkillCard>();
+    @type([SkillCard]) player1SkillState: SkillCard[] = [];
+    @type([SkillCard]) player2SkillState: SkillCard[] = [];
+    @type([SkillCard]) player1RandomSkill: SkillCard[] = [];
+    @type([SkillCard]) player2RandomSkill: SkillCard[] = [];
 }
