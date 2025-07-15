3d8976a551b93b3b3c1f26fa55255e9dc137c3f7
otake1006 <san-j23008@sangi.jp>
2025-07-15 13:20:59 +0900

    弱体の名称変更

--- backend/src/rooms/schema/buff.ts
+++ backend/src/rooms/schema/buff.ts
@@ -3,6 +3,6 @@ export class buff extends Schema {
     @type('number') muscular: number = 0; //筋肉
     @type('number') guard: number = 0; //ガード値
     @type('number') brittle: number = 0; //脆い
-    @type('number') Weak: number = 0; //弱体
+    @type('number') weaknes: number = 0; //弱体
     @type('number') poison: number = 0; //毒
 }
