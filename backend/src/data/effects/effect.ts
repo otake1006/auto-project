bd3a8bf4a940f43a29eb131e3f6edcfb91372f5d
otake1006 <san-j23008@sangi.jp>
2025-07-25 23:36:10 +0900

    関数をbuffに移動

--- backend/src/data/effects/effect.ts
+++ backend/src/data/effects/effect.ts
@@ -1,5 +1,4 @@
 import { Schema, type, ArraySchema } from '@colyseus/schema';
-import { Player } from '../../rooms/schema/MyRoomState';
 export type Type = 'muscular' | 'guard' | 'brittle' | 'weaknes' | 'poison';
 
 export class effectCard extends Schema {
@@ -9,25 +8,5 @@ export class effectCard extends Schema {
     constructor(init?: Partial<effectCard>) {
         super();
         Object.assign(this, init);
-    }
-
-    public getBuff(player: Player) {
-        switch (this.type) {
-            case 'muscular':
-                player.buffs.muscular += this.power;
-                break;
-            case 'guard':
-                player.buffs.guard += this.power;
-                break;
-            case 'brittle':
-                player.buffs.brittle += this.power;
-                break;
-            case 'weaknes':
-                player.buffs.weaknes += this.power;
-                break;
-            case 'poison':
-                player.buffs.poison += this.power;
-                break;
-        }
     }
 }
